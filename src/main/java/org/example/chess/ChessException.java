8dd9f10c16e4549e78c8cef8c845fa148203241c
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-05-21 22:24:13 -0300

    ChessException and ChessPosition

---
+++ src/main/java/org/example/chess/ChessException.java
@@ -0,0 +1,11 @@
+package org.example.chess;
+
+public class ChessException extends RuntimeException{
+
+    private static final long srialVersionUID = 1L;
+
+    public ChessException (String msg){
+        super(msg);
+    }
+
+}
